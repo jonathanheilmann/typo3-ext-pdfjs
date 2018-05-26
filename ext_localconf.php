9ccdb3122cc9c656f7944e565eb6205cb1b4618e
Jonathan Heilmann <mail@jonathan-heilmann.de>
2018-05-26 21:09:12 +0200

    [TASK] Add Support for Typo 3 v8.x (Related to #7)
    Added icon to icon registry for new content element wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,12 +10,21 @@ if (!defined('TYPO3_MODE'))
 	)
 );
 
+// Register icon
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    'tx-pdfjs-pdfviewer',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:' . $_EXTKEY . '/ext_icon.svg']
+);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     'mod.wizards.newContentElement.wizardItems {
         plugins {
             elements {
                 tx_pdfjs_pdfviewer {
                     icon = ' . \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.svg
+                    iconIdentifier = tx-pdfjs-pdfviewer
                     title = LLL:EXT:pdfjs/Resources/Private/Language/newContentElement.xlf:pdfViewer.title
                     description = LLL:EXT:pdfjs/Resources/Private/Language/newContentElement.xlf:pdfViewer.description
                     tt_content_defValues {
