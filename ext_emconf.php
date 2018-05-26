319748e667338e92370ae1e042264a7f5109bb71
Jonathan Heilmann <mail@jonathan-heilmann.de>
2018-05-26 21:09:42 +0200

    [TASK] Add Support for Typo 3 v8.x (Related to #7)
    Updated dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'php' => '',
-      'typo3' => '7.6.0-7.6.99',
+      'typo3' => '7.6.0-8.7.99',
     ),
     'conflicts' => 
     array (
