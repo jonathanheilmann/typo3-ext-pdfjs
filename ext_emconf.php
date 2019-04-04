ca56cde28ccc6336f7c9d7b000320406d550191a
Jonathan Heilmann <mail@jonathan-heilmann.de>
2019-04-04 21:25:48 +0200

    [TASK] Support TYPO3 9 LTS (Related to #15)
    Updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'php' => '',
-      'typo3' => '7.6.0-8.7.99',
+      'typo3' => '8.7.0-9.5.99',
     ),
     'conflicts' => 
     array (
